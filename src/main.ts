31de70837665f08257ebf68a55d1d6376e8d522d
Rubick <rubick.ua@gmail.com>
2021-07-31 19:25:07 +0300

    delete app.service-controller

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('', app, document);
 
   await app.listen(3001);
   console.log(`Application is running on: ${await app.getUrl()}`);
